095e07cf8120a094e5c44f732f3c407438416361
ixiivi <spyjae@naver.com>
2025-05-10 02:07:28 +0900

    setting

---
+++ Source/Space S/Space_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_SApp.swift
+//  Space S
+//
+//  Created by 김재현 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Space_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Space S/Network/APIEndpoints.swift
@@ -0,0 +1,7 @@
+//
+//  APIEndpoint.swift
+//  Space S
+//
+//  Created by 김재현 on 5/9/25.
+//
+
