6e18cb6afe3de625640f633b25eda77d7bc19b68
ixiivi <spyjae@naver.com>
2025-05-09 22:57:21 +0900

    Initial Commit

---
+++ Space STests/Space_STests.swift
@@ -0,0 +1,17 @@
+//
+//  Space_STests.swift
+//  Space STests
+//
+//  Created by 김재현 on 5/9/25.
+//
+
+import Testing
+@testable import Space_S
+
+struct Space_STests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
